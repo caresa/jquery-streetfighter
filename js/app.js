824ad7f9da1dfdf62893e2c5138edbdb2e4ca5ec
Caresa <unimportantfact@gmail.com>
2014-04-01 19:35:47 -0500

    Added new images added fade in on intro and animation on logo

--- js/app.js
+++ js/app.js
@@ -1,31 +1,53 @@
 $(document).ready(function() {
+
+  $('.ryu').hide();
+
+  $( ".logo" ).animate({ opacity: 1, left: "450", height: "toggle" },  2000, function() {    
+    $('.logo').fadeOut('fast').finish();
+    $('.ryu').show();
+    $('.intro').fadeIn(4000);
+  });
+
+ 
+
  $('.ryu').mouseenter(function() {
-  $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  })
-	.mouseleave(function() {
-	$('.ryu-ready').hide();
-	$('.ryu-still').show();
-	})
-	.mousedown(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  }).mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  }).mousedown(function() {
     playHadouken(); 
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').finish().show()
-    .animate(
-  {'left': '300px'},
-  500,
-  function() {
-    $(this).hide();
-    $(this).css('left', '-212px');
-  }
-);
-  })
-  .mouseup(function() {
-  $('.ryu-throwing').hide();
-  $('.ryu-ready').show();
+    $('.hadouken').finish().show().animate({'left': '300px'},500,function() {
+      $(this).hide();
+      $(this).css('left', '-212px');
+    });
+  }).mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
+
+  $('body').keydown(function(e){
+    if (event.which == 88) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+        // $('.ryu-still').hide();
+      $('.ryu-cool').show();
+    }
+  }).keyup(function(e){
+    if (event.which == 88){
+      $('.ryu-cool').hide();
+      $('.ryu-ready').show();
+      // $('.ryu-still').show();
+    }
+  });
+
 });
+
+
+
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
